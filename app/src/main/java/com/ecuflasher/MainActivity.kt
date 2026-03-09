e23c641e5b4371dd63e087e28e3511229aeb5284
starlingcrossgte <starlingcrossgte@gmail.com>
2026-03-09 14:10:58 -0400

    replace MainActivity with basic Activity

--- app/src/main/java/com/ecuflasher/MainActivity.kt
+++ app/src/main/java/com/ecuflasher/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.ecuflasher
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
