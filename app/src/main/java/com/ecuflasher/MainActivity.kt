7bdf68532a26760747773808bf38e701ac6c7ac6
starlingcrossgte <starlingcrossgte@gmail.com>
2026-03-12 18:46:33 -0400

    Restore UI layout loading in MainActivity

--- app/src/main/java/com/ecuflasher/MainActivity.kt
+++ app/src/main/java/com/ecuflasher/MainActivity.kt
@@ -8,6 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContentView(R.layout.activity_main)
+
         EcuLogger.main("ECUFlasher started")
 
         val usbManager = UsbDeviceManager(this)
