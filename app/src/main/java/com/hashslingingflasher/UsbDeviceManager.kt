01c5b249f97ae28f63349042a9942cfb7f28c09d
starlingcrossgte <starlingcrossgte@gmail.com>
2026-03-20 19:59:48 -0400

    Set K-line baud command to 4800 before SSM TCM probe

--- app/src/main/java/com/hashslingingflasher/UsbDeviceManager.kt
+++ app/src/main/java/com/hashslingingflasher/UsbDeviceManager.kt
@@ -231,15 +231,21 @@ class UsbDeviceManager(private val context: Context) {
 
         EcuLogger.usb("Subaru SSM K-line command requested: $command")
 
-        val baudResult = TactrixTestResult(
-            success = true,
-            statusMessage = "Skipped K-line ISO baud command",
-            bytesSent = 0,
-            bytesReceived = 0,
-            responseHex = "",
-            responseAscii = ""
+        val baudResult = sendKlineAsciiCommand(
+            session = session,
+            commandLabel = "OpenPort K-line ISO baud command",
+            commandString = "atib 48\r\n"
         )
-
+        if (looksLikeAdapterRejection(baudResult.responseAscii)) {
+            return TactrixTestResult(
+                false,
+                "OpenPort rejected K-line ISO baud command",
+                baudResult.bytesSent,
+                baudResult.bytesReceived,
+                baudResult.responseHex,
+                baudResult.responseAscii
+            )
+        }
 
         val initAddrResult = sendKlineAsciiCommand(
             session = session,
